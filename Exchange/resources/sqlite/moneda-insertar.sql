7cf143996a82c1dfa367b93b83d49d40218958d1
gdborja <bghackter@gmail.com>
2021-05-21 21:15:06 +0100

    21/05/2021 Aumento de la db moneda

--- Exchange/resources/sqlite/moneda-insertar.sql
+++ Exchange/resources/sqlite/moneda-insertar.sql
@@ -18,3 +18,112 @@ VALUES ('Tether','USDT',1.0006);
 
 INSERT INTO Moneda(nombreMoneda,ticket,valor)
 VALUES ('Ripple','XRP',1.5125);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Litecoin','LTC',176.94);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Solana','SOL',38.30);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Monero','XMR',262.76);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Maker','MKR',3249.30);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Dash','DASH',182.73);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Zcash','ZEC',150.92);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Venus','XVS',36.53);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Sora','XOR',433.90);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Gnosis','GNO',135.57);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Aragon','ANT',3.33);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Serum','SRM',4.28);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Neo','NEO',54.46);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Kusama','KSM',303.58);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Holo','HOT',0.00072);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Ravencoin','RVN',0.0798);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('WhiteCoin','XWC',1.35);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('NXM','NXM',89.63);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Amp','AMP',0.04032);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('MonaCoin','MONA',1.90);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('TROY','TROY',0.00988);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Ultra','UOS',0.2702);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Verasity','VRA',0.01768);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Fusion','FSN',0.9556);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Zenon','ZNN',11.02);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Constellation','DAG',0.04816);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Attila','ATT',0.1151);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Oxen','OXEN',0.01768);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Glitch','GLCH',0.8327);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('CyberVein','CVT',0.04846);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Homeros','HMR',0.09853);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Beldex','BDX',0.0503);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Vertcoin','VTC',0.7738);
+
+INSERT INTO Moneda(nombreMoneda,ticket,valor)
+VALUES ('Pangolin','PNG',2.06);
+
+
+
+
+
+
+
+
+
+
