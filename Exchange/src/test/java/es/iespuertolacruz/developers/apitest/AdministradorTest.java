de253e546387f2cbaf6ff8ae5b6ee4156e59b5fd
Daniel <dany_am95@hotmail.com>
2021-05-28 20:01:09 +0100

    Creando Test de api

--- Exchange/src/test/java/es/iespuertolacruz/developers/apitest/AdministradorTest.java
+++ Exchange/src/test/java/es/iespuertolacruz/developers/apitest/AdministradorTest.java
@@ -1,35 +1,44 @@
 package es.iespuertolacruz.developers.apitest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import es.iespuertolacruz.developers.api.Administrador;
+import es.iespuertolacruz.developers.api.DatosPersonales;
+import es.iespuertolacruz.developers.api.Direccion;
+import es.iespuertolacruz.developers.api.Tarjeta;
 
 public class AdministradorTest {
 
     Administrador administrador;
     Administrador administrador2;
+    DatosPersonales datosPersonales;
+    DatosPersonales datosPersonales2;
+    Direccion direccion;
+    Tarjeta tarjeta;
 
     @BeforeEach
     public void setUp(){
-        administrador = new Administrador(uid, datosPersonales, email, contrasenia, direccion, tarjeta);
+        datosPersonales = new DatosPersonales("456987A", "cristian", "apellidos2", 54);
+        administrador = new Administrador("1002", datosPersonales, "administrador@gmail.com", "*********");
     }
 
     @Test
     public void validarTest(){
+        datosPersonales2 = new DatosPersonales("6548a", "yoquese", "apellidos3", 96);
         administrador = new Administrador();
-        administrador.setUid(uid);
+        administrador.setUid("1003");
         administrador.setDatosPersonales(datosPersonales);
-        administrador.setEmail(email);
-        administrador.setContrasenia(contrasenia);
+        administrador.setEmail("admin@gmail.com");
+        administrador.setContrasenia("123456");
         
-
-        assertEquals(administrador.getUid(), actual, "El campo no coincide");
-        assertEquals(administrador.getDatosPersonales(), actual, "El campo no coincide");
-        assertEquals(administrador.getEmail(), actual, "El campo no coincide");
-        assertEquals(administrador.getContrasenia(), actual, "El campo no coincide");
+        assertEquals(administrador.getUid(), "1003", "El campo no coincide");
+        assertEquals(administrador.getDatosPersonales(), datosPersonales2, "El campo no coincide");
+        assertEquals(administrador.getEmail(), "admin@gmail.com", "El campo no coincide");
+        assertEquals(administrador.getContrasenia(), "123456", "El campo no coincide");
     }
 
     @Test
@@ -46,10 +55,10 @@ public class AdministradorTest {
     @Test
     public void equalErrorTest(){
         administrador2 = administrador;
-        administrador.setUid(uid);
+        administrador.setUid("1003");
         administrador.setDatosPersonales(datosPersonales);
-        administrador.setEmail(email);
-        administrador.setContrasenia(contrasenia);
+        administrador.setEmail("admin@gmail.com");
+        administrador.setContrasenia("123456");
 
         assertTrue(administrador.equals(administrador2), "Los objetos no deben ser iguales");
     }
