e41f30be874f21de84dcad054b3eee1d679f8464
gdborja <bghackter@gmail.com>
2021-05-30 21:41:14 +0100

    Insertar test y corección de errores

--- Exchange/src/test/java/es/iespuertolacruz/developers/MiembroControllerTest.java
+++ Exchange/src/test/java/es/iespuertolacruz/developers/MiembroControllerTest.java
@@ -18,6 +18,7 @@ import es.iespuertolacruz.developers.api.Tarjeta;
 import es.iespuertolacruz.developers.controller.DatosPersonalesController;
 import es.iespuertolacruz.developers.controller.DireccionController;
 import es.iespuertolacruz.developers.controller.MiembroController;
+import es.iespuertolacruz.developers.controller.MonedaController;
 import es.iespuertolacruz.developers.controller.TarjetaController;
 import es.iespuertolacruz.developers.excepcion.BbddException;
 import es.iespuertolacruz.developers.excepcion.DatosPersonalesException;
@@ -30,53 +31,53 @@ import es.iespuertolacruz.developers.modelo.MiembroModelo;
 
 public class MiembroControllerTest extends UtilidadesTest {
 
-    
     MiembroController miembroController;
     MiembroModelo miembroModelo;
     DireccionController direccionController;
     TarjetaController tarjetaController;
     DatosPersonalesController datosPersonalesController;
+    MonedaController monedaController;
 
     Miembro miembro;
     DatosPersonales datosPersonales;
     Direccion direccion;
     Tarjeta tarjeta;
-    
-   
 
     @BeforeEach
     public void setUp() throws BbddException, FicheroException, SQLException {
-        if ( datosPersonalesController == null) {
+        if (datosPersonalesController == null) {
             datosPersonalesController = new DatosPersonalesController();
         }
 
-        if ( direccionController == null) {
+        if (direccionController == null) {
             direccionController = new DireccionController();
         }
 
-        if ( tarjetaController == null) {
+        if (tarjetaController == null) {
             tarjetaController = new TarjetaController();
         }
 
         if (miembroController == null) {
             miembroController = new MiembroController();
         }
-       
-      miembro =  crearMiembro(null,null,null,null,null,null,null);
-         
-           
+
+        if (monedaController == null) {
+            monedaController = new MonedaController();
+        }
+
+
     }
 
     @AfterEach
     public void after() {
-        
-           try {
+
+        try {
             direccionController.eliminar(direccion);
             tarjetaController.eliminar(tarjeta);
             datosPersonalesController.eliminar(datosPersonales);
             miembroController.eliminar(miembro);
         } catch (DireccionException e) {
-           fail(e.getMessage());
+            fail(e.getMessage());
         } catch (BbddException e) {
             fail(e.getMessage());
         } catch (TarjetaException e) {
@@ -85,41 +86,53 @@ public class MiembroControllerTest extends UtilidadesTest {
             fail(e.getMessage());
         } catch (MiembroException e) {
             fail(e.getMessage());
-            
+
         }
-    
+
     }
 
     @Test
-    public void validarClienteNullTest() {
-            try {
-                tarjeta = crearTarjeta();
-                tarjetaController.insertar(tarjeta);
-                direccion = crearDireccion();
-                direccionController.insertar(direccion);
-                datosPersonales = crearDatosPersonales();
-                datosPersonalesController.insertar(datosPersonales);
-                miembro = crearMiembro();
-                
-               
-                miembroController.insertar(miembro);
+    public void insertarTest() {
+        try {
+            tarjeta = crearTarjeta();
+            tarjetaController.insertar(tarjeta);
+            direccion = crearDireccion();
+            direccionController.insertar(direccion);
+            datosPersonales = crearDatosPersonales();
+            datosPersonalesController.insertar(datosPersonales);
+            miembro = crearMiembro();
 
-            } catch (MiembroException e) {
-                fail(e.getMessage());
-            } catch (BbddException e) {
-               fail(e.getMessage());
-            } catch (DatosPersonalesException e) {
-                fail("datospersonales");
-            } catch (TarjetaException e) {
-               fail("tarjeta");
-            } catch (DireccionException e) {
-                fail("direccion");
-            }
-       
+            miembroController.insertar(miembro);
+
+        } catch (MiembroException e) {
+            fail(e.getMessage());
+        } catch (BbddException e) {
+            fail(e.getMessage());
+        } catch (DatosPersonalesException e) {
+            fail("datospersonales");
+        } catch (TarjetaException e) {
+            fail("tarjeta");
+        } catch (DireccionException e) {
+            fail("direccion");
+        }
+
     }
-   
-     
-   
+
+
+    @Test
+    public void insertarMonedaTest() {
+        moneda = crearMoneda();
+       monedaController.insertar(moneda);
+
+    }
+
+
+
+
+    private Object crearMoneda() {
+        return null;
+    }
+
     private Direccion crearDireccion() {
         return new Direccion("1111e", "38400", "La laja", "a45", "SC/ Tenerife", "Espania");
     }
@@ -129,13 +142,13 @@ public class MiembroControllerTest extends UtilidadesTest {
 
     }
 
-    private Tarjeta crearTarjeta(){
-        return new Tarjeta("1111e-aaaa","Borja","20/5/2090",108);
+    private Tarjeta crearTarjeta() {
+        return new Tarjeta("1111e-aaaa", "Borja", "20/5/2090", 108);
     }
 
-
-    private Miembro crearMiembro(){
-        return new Miembro("10000","admin",crearDatosPersonales(),"es@email.com","1234",crearDireccion(),crearTarjeta());
+    private Miembro crearMiembro() {
+        return new Miembro("10000", "admin", crearDatosPersonales(), "es@email.com", "1234", crearDireccion(),
+                crearTarjeta());
     }
 
 }
